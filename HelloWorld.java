347148b53baf4026ca9765c43fb5eb128ca07bea
Karolina Cizaite <kcizaite@gmail.com>
2022-09-30 18:29:59 +0300

    master branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
 class HelloWorld {
 	public static void main (String[] args) {
-		System.out.prinln("Hello Wolrd");
-		System.out.println("HEllo Wolrd")
+		System.out.println("Hello Wolrd!");
 	}
 }
